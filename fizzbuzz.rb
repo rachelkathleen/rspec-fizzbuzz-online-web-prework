3a23a5c107922d3de8a4b38671b2c431ef5bd225
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-02 04:05:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0  && int % 5 != 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  elsif int % 5 == 0 
+  elsif int % 5 == 0
     "Buzz"
   elsif int % 15 == 0
     "FizzBuzz"
   else
-    
+
   end
 end
