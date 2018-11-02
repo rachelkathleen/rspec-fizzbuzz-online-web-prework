801da0e6f550ea916ddbb18d5da4fc36a714236b
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-02 03:58:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,5 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
-
-def fizzbuzz(x)
-  if x % 3 == 1 # if the number int is divisible by 3
-    return "Fizz" # Go fizz
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  end
 end
